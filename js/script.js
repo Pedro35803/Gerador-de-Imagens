8bc3e5d05e973422d18a72e1500227ee751300b2
Pedro <pedrojuniorifpb@gmail.com>
2022-03-22 00:47:28 -0300

    Favoritos e random funcionando

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-let containerImg = document.querySelector("#linkImg");
-let linkImg = document.querySelector("#directLink");
+let linkDireto = document.querySelector("#directLink");
 let imgHeart = document.querySelector("#heart");
+let linkImg = document.querySelector("#linkImg");
+let containePai = document.querySelector("#containerPaiImg");
+let cont = 0;
 
 // atualizaImagem();
 
-document.querySelector("#buttonImgs").addEventListener("click", () => atualizaImagem());
+document.querySelector("#buttonImgs").addEventListener("click", () => imgRandom());
+document.querySelector("#buttonFavorites").addEventListener("click", () => imgFavorita());
 
 imgHeart.addEventListener("click", () => {
-    const indice = favorites.indexOf(linkImg.href);
+    const lista = [linkImg.style.background, linkDireto.href]
+    // const indice = favorites.indexOf(linkImg.style.background);
+    const indice = favorites.indexOf(lista);
+
     if (indice == -1) {
         imgHeart.src = "../img/heart_red.svg"
-        favorites.push(linkImg.href);
+        favorites.push(lista);
     } else {
         imgHeart.src = "../img/heart.svg"
         favorites.splice(indice, 1);
@@ -19,15 +25,40 @@ imgHeart.addEventListener("click", () => {
     localStorage.favorites = JSON.stringify(favorites);
 });
 
-function atualizaImagem() {
+function imgRandom() {
+    let classe = containePai.classList;
+    if (classe.contains("fundoRetratoInvertido")) {
+        classe.remove("fundoRetratoInvertido");
+        classe.add("fundoRetrato");
+    }
+    atualizaImg();
+}
+
+function atualizaImg() {
     let apiKey = "kaokzJCbcYsVY9jm5V2tjN4nJ39YEP4rCmn8uZiWqxQ";
     imgHeart.src = "../img/heart.svg"
 
     fetch("https://api.unsplash.com/photos/random/?client_id=" + apiKey)
     .then((response) => response.json())
     .then((dados) => {
-        containerImg.style.background = "url(" + dados.urls.raw + "&h=450&w=375&fit=crop=faces,center&fit=fill&fill=blur&auto=compress) center no-repeat";
-        linkImg.href = dados.links.html;
+        linkImg.style.background = "url(" + dados.urls.raw + "&h=450&w=375&fit=crop=faces,center&fit=fill&fill=blur&auto=compress) center no-repeat";
+        linkDireto.href = dados.links.html;
     })
     .catch((error) => alert("Bateu o limite de imagens por minutos: " + error));
+}
+
+function imgFavorita() {
+    let classe = containePai.classList;
+    if (classe.contains("fundoRetrato")) {
+        classe.remove("fundoRetrato");
+        classe.add("fundoRetratoInvertido");
+    }
+
+    console.log(favorites);
+    linkImg.style.background = favorites[cont][0];
+    linkDireto.href = favorites[cont][1];
+    cont++;
+    if (cont == favorites.length) {
+        cont = 0;
+    }
 }
