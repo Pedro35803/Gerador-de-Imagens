4cc8a617e6a7ef18f2c610dec9fc1c8e2f183621
Pedro <pedrojuniorifpb@gmail.com>
2022-03-22 18:49:22 -0300

    Adicionando o stile dos butões

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ document.querySelector("#buttonFavorites").addEventListener("click", () => imgFa
 
 imgHeart.addEventListener("click", () => {
     const lista = [linkImg.style.background, linkDireto.href]
-    // const indice = favorites.indexOf(linkImg.style.background);
     const indice = favorites.indexOf(lista);
 
     if (indice == -1) {
@@ -41,24 +40,34 @@ function atualizaImg() {
     fetch("https://api.unsplash.com/photos/random/?client_id=" + apiKey)
     .then((response) => response.json())
     .then((dados) => {
-        linkImg.style.background = "url(" + dados.urls.raw + "&h=450&w=375&fit=crop=faces,center&fit=fill&fill=blur&auto=compress) center no-repeat";
-        linkDireto.href = dados.links.html;
+        let link = "url(" + dados.urls.raw + "&h=450&w=375&fit=crop=faces,center&fit=fill&fill=blur&auto=compress) center no-repeat";
+        mostrarImagem(link, dados.links.html);
     })
     .catch((error) => alert("Bateu o limite de imagens por minutos: " + error));
 }
 
 function imgFavorita() {
     let classe = containePai.classList;
+    
     if (classe.contains("fundoRetrato")) {
         classe.remove("fundoRetrato");
         classe.add("fundoRetratoInvertido");
     }
 
-    console.log(favorites);
-    linkImg.style.background = favorites[cont][0];
-    linkDireto.href = favorites[cont][1];
+    if (localStorage > 0) {
+        mostrarImagem(favorites[cont][0], favorites[cont][1]);
+    } else {
+        mostrarImagem("#50858B", "");
+        imgHeart.src = "../img/heart.svg";
+    }
+
     cont++;
     if (cont == favorites.length) {
         cont = 0;
     }
+}
+
+function mostrarImagem(planoDeFundo, linkUnsplash) {
+    linkImg.style.background = planoDeFundo;
+    linkDireto.href = linkUnsplash;
 }
