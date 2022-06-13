83275078b690144207307c1b5ece99e0f77a0b41
Pedro <pedrojuniorifpb@gmail.com>
2022-06-13 15:56:03 -0300

    Apagando a pagina sobre e arquivos não utilizados

--- js/about.js
+++ js/about.js
@@ -5,17 +5,3 @@ fetch("http://localhost:3000/")
         conteudoContainerMembro(link, dados[0].name, dados[0].description);
     })
     .catch(() => alert("Ligue o Node.Js para poder ver quem criou a pagina"));
-
-function conteudoContainerMembro(linkImg, textH2, textP) {
-    let article = document.querySelector("#about-member");
-    let figure = document.querySelector("#sobre-img");
-    let h2 = document.createElement("h2");
-    let p = document.createElement("p");
-
-    figure.style.background = linkImg;
-    h2.textContent = textH2;
-    p.textContent = textP;
-
-    article.appendChild(h2);
-    article.appendChild(p);
-}
--- js/app.js
+++ js/app.js
@@ -3,12 +3,6 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
-let grupo = [
-    {name: "Pedro Júnior",
-    img: "./img/minha_foto.jpg",
-    description: "Estudante de ADS e progamador back-end, resposavel por todo o projeto"}
-]
-
 app.use(cors());
 app.get('/', (req, res) => {
 	res.send(grupo);
