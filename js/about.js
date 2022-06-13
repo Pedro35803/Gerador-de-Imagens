71abf7aede6646001e9edf0c1f0dfb3bb578a270
Pedro <pedrojuniorifpb@gmail.com>
2022-06-13 16:02:30 -0300

    Adicionando o .gitignore e criando uma pasta em que vou adicionar o backend

--- js/about.js
+++
@@ -1,7 +0,0 @@
-fetch("http://localhost:3000/")
-    .then((response) => response.json())
-    .then((dados) => {
-        let link = "url(" + dados[0].img + ")";
-        conteudoContainerMembro(link, dados[0].name, dados[0].description);
-    })
-    .catch(() => alert("Ligue o Node.Js para poder ver quem criou a pagina"));
