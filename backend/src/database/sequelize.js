ec704bedcc9488319055d60c6d839d1d0f73ac83
Pedro <pedrojuniorifpb@gmail.com>
2022-06-17 18:02:33 -0300

    Se comunicando com sucesso com o banco de dados

--- backend/src/database/sequelize.js
+++ backend/src/database/sequelize.js
@@ -1,30 +1,14 @@
 const { Sequelize } = require("sequelize");
+require('dotenv').config();
 
 const hostDoBD = String(process.env.DATABASE_URL);
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//             rejectUnauthorized: false,
-//         },
-//     },
-// });
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(hostDoBD, {
     dialectOptions: {
         ssl: {
             rejectUnauthorized: false,
         },
     },
 });
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL)
-
-sequelize
-    .authenticate()
-    .then(() => console.log("Conexão com o BD feita com sucesso"))
-    .catch((error) => console.log("Ocorreu algum error ao se conectar com o BD: " + error));
 
 module.exports = sequelize;
